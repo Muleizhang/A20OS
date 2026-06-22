d997f9259bb4bd99ad1560554bb438f9690a4dea
Mulei Zhang <muleizh@outlook.com>
2026-06-22 15:37:27 +0800

    expand bounded ltp local fs tail

--- user/contest_init/contest.sh
+++ user/contest_init/contest.sh
@@ -509,7 +509,8 @@ run_ltp_bounded_subset() {
         af_alg02 af_alg03 af_alg05 af_alg06 \
         cve-2017-17052 \
         print_caps tst_ansi_color.sh tst_exit tst_hexdump \
-        clone01 diotest1 diotest4 memcontrol01 mmap001
+        clone01 diotest1 diotest4 memcontrol01 mmap001 \
+        dirty doio epoll-ltp fs_racer.sh
     do
         run_ltp_case_with_timeout "$name" 60 || (( failed_cases++ ))
     done
