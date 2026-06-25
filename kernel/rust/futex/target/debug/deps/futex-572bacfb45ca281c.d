f33294355a7bbb9564b938c29a5d44a12b1b4c29
fQwQf <supertjz123@foxmail.com>
2026-06-25 17:36:41 +0800

    rust: rewrite sys_futex.c in Rust and enable by default

---
+++ kernel/rust/futex/target/debug/deps/futex-572bacfb45ca281c.d
@@ -0,0 +1,6 @@
+/home/fq/project/oskernel2025-a20/kernel/rust/futex/target/debug/deps/libfutex-572bacfb45ca281c.rmeta: lib.rs ffi.rs
+
+/home/fq/project/oskernel2025-a20/kernel/rust/futex/target/debug/deps/futex-572bacfb45ca281c.d: lib.rs ffi.rs
+
+lib.rs:
+ffi.rs:
